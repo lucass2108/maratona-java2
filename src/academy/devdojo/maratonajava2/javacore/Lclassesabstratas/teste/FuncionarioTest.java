ad8e5e1a55f5b2e0938c6cfb6f69d310cdee36e7
Lucas Corrêa <correasilva2007@gmail.com>
2025-12-23 12:30:12 -0300

    Refazendo arquivos IV.I

--- src/academy/devdojo/maratonajava2/javacore/Lclassesabstratas/teste/FuncionarioTest.java
+++ src/academy/devdojo/maratonajava2/javacore/Lclassesabstratas/teste/FuncionarioTest.java
@@ -5,11 +5,16 @@ import academy.devdojo.maratonajava2.javacore.Lclassesabstratas.dominio.Gerente;
 
 public class FuncionarioTest {
 
+    //teste
+
     static void main(String[] args) {
         Gerente gerente = new Gerente("Tanjiro", 5000);
         System.out.println(gerente);
 
         Desenvolvedor desenvolvedor = new Desenvolvedor("Einsen", 12000);
         System.out.println(desenvolvedor);
+
+        System.out.println("Teste");
+
     }
 }
