c73e05f2ea6cb06a6af9bcb21ccbbe5409651be2
Lucas Corrêa <correasilva2007@gmail.com>
2025-12-24 16:33:39 -0300

    Salvando estudos

--- src/academy/devdojo/maratonajava2/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
+++ src/academy/devdojo/maratonajava2/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class RuntimeExceptionTest04 {
     /**
-     *Deve ser ressaltado que as excessões são objetos com herança,
+     * Deve ser ressaltado que as excessões são objetos com herança,
      * por isso pode-se usar uma excessão mais genérica.
      * Por exemplo: a ArrayIndexOutBoundsException é filha de RuntimeException, assim quando se trata a
      * RuntimeException tbm se trata a ArrayIndexOutBoundsExceptions e outros filhos e netos e por assim segue.
@@ -15,26 +15,22 @@ public class RuntimeExceptionTest04 {
     static void main(String[] args) {
         try {
             throw new RuntimeException();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException");
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("Dentro do IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Dentro do IllegalArgumentException");
-        } catch (ArithmeticException e) {
-            System.out.println("Dentro do ArithmeticException");
-        } catch (RuntimeException e){
+        } catch (RuntimeException e) {
             System.out.println("Dentro do RuntimeException");
         }
 
         try {
             talvezLanceException();
-        } catch (Exception e){
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private static void talvezLanceException() throws SQLException, FileNotFoundException {
+    private static void talvezLanceException() throws SQLException, FileNotFoundException, IOException {
 
     }
 }
