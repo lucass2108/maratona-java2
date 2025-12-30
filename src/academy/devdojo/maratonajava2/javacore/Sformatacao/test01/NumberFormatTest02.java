50f2967e9483a43a8da2dd2f3a7311a9d6706aa1
Lucas Corrêa <correasilva2007@gmail.com>
2025-12-30 13:05:56 -0300

    Formatação em Java

---
+++ src/academy/devdojo/maratonajava2/javacore/Sformatacao/test01/NumberFormatTest02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava2.javacore.Sformatacao.test01;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatTest01 {
+    static void main(String[] args) {
+        Locale localeDefault = Locale.getDefault();
+        Locale localePT = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIt = Locale.ITALY;
+        NumberFormat[] nfa = new NumberFormat[]{NumberFormat.getInstance(), NumberFormat.getInstance(localeJP),
+                NumberFormat.getInstance(localePT), NumberFormat.getInstance(localeIt)};
+        double valor = 10_000.2130;
+        for (NumberFormat numberFormat : nfa) {
+            System.out.println(numberFormat.format(valor));
+        }
+
+    }
+}
---
+++ src/academy/devdojo/maratonajava2/javacore/Sformatacao/test01/SimpleDateFormatTest01.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonajava2.javacore.Sformatacao.test01;
+
+public class SimpleDateFormatTest01 {
+}
