8949f7934e9ed9e1de564f045a22e24df2dbb8ba
Lucas Corrêa <correasilva2007@gmail.com>
2026-01-06 11:20:45 -0300

    Classes Utilitárias: ResourceBundle

---
+++ src/academy/devdojo/maratonajava2/javacore/Tresourcebundle/teste/ResourceBundleTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava2.javacore.Tresourcebundle.teste;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class ResourceBundleTest01 {
+    static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+
+        ResourceBundle bundle = ResourceBundle.getBundle("mensagem", new Locale("en", "US"));
+        boolean sasa = bundle.containsKey("sasa");
+        System.out.println(sasa);
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+
+        bundle = ResourceBundle.getBundle("mensagem", new Locale("pt", "BR"));
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+
+        System.out.println(bundle.getString("hi"));
+
+    }
+}
